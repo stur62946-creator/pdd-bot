0fbc08d33fbcf6c5a54bbefda9a9285ca4dca485
stur62946-creator <stur62946@gmail.com>
2026-08-02 13:38:33 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,10 @@ const BOT_TOKEN = 'f9LHodD0cOKD36Dt6aXPSyuvzh1cr95O6kcyGcB0AMiHHxtKZj2Fy_q6xF8uU
 
 const bot = new Bot(BOT_TOKEN);
 
-// Обработчик команды '/start'
 bot.command('start', (ctx) => {
   ctx.reply('✅ Бот работает! Привет!');
 });
 
-// Запускаем бота
 bot.start();
 
-console.log('🤖 Бот запущен на официальном SDK. Жду /start...');
+console.log('🤖 Бот запущен (официальный пакет). Жду /start...');
